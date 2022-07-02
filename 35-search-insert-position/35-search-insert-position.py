05c7d631e23e32e7a435426ca9697aea958deb08
Asliddin <98747380+asliddin2002developer@users.noreply.github.com>
2022-07-02 08:01:18 +0500

    Time: 86 ms (31.56%), Space: 14.8 MB (40.72%) - LeetHub

--- 35-search-insert-position/35-search-insert-position.py
+++ 35-search-insert-position/35-search-insert-position.py
@@ -1,13 +1,36 @@
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
-        ## Linear Search
         
+        
+        ## Binary Search
+        ## Time Complexity: O(log n)
+        ## Space Complexity: O(1)
+        low = 0 
+        high = len(nums) - 1
         index = 0
-        for i, v in enumerate(nums):
-            if v == target:
-                return i 
-            if v < target:
-                index = i+1
-        return index   
+        
+        while low <= high:
+            mid = (high+low) // 2
+            
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] > target:
+                high = mid - 1
+            else:
+                low = mid + 1
+                index = mid+1
+        return index        
+        
+        ## Linear Search
+        ## Time Complexity: O(n)
+        ## Space Complexity: O(1)
+        
+        # index = 0
+        # for i, v in enumerate(nums):
+        #     if v == target:
+        #         return i 
+        #     if v < target:
+        #         index = i+1
+        # return index   
                             
             
