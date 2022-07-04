f692ae1d98f80cf0384599d39ce3447fdbdd2dd6
Asliddin <98747380+asliddin2002developer@users.noreply.github.com>
2022-07-04 10:40:15 +0500

    Time: 139 ms (87.37%), Space: 15.1 MB (10.97%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.py
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.py
@@ -1,6 +1,16 @@
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
         
+        # Brute Force 
+        # Time Complexity: O(n2)
+        # Space Comlexity: O(1)
+        # for i in range(len(numbers)):
+        #     for j in range(i+1, len(numbers)):
+        #         if numbers[i]+numbers[j] == target:
+        #             return [i+1, j+1]
+        #         elif numbers[i]+numbers[j] > target:
+        #             continue
+        
         # Time Complexity: O(n)
         # Space Complexity: O(1) 
             
@@ -18,9 +28,10 @@ class Solution:
         l, r = 0, len(numbers)-1
         
         while l < r:
-            if numbers[l] + numbers[r] == target:
+            curSum = numbers[l] + numbers[r]
+            if curSum == target:
                 return [l+1, r+1]
-            elif numbers[l] + numbers[r] > target:
+            elif curSum > target:
                 r -= 1
             else:
                 l += 1
