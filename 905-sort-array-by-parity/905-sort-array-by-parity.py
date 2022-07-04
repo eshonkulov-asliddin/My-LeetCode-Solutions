0f3f135ce4dece00ea93d7c25172bf7d45d10586
Asliddin <98747380+asliddin2002developer@users.noreply.github.com>
2022-07-04 12:12:43 +0500

    Time: 149 ms (21.46%), Space: 14.6 MB (96.18%) - LeetHub

--- 905-sort-array-by-parity/905-sort-array-by-parity.py
+++ 905-sort-array-by-parity/905-sort-array-by-parity.py
@@ -1,5 +1,9 @@
 class Solution:
     def sortArrayByParity(self, nums: List[int]) -> List[int]:
+        # Two Pointer
+        # Time Complexity: O(n)
+        # Space Complexity: O(1)
+        
         l = 0
         for r in range(len(nums)):
             if nums[r] % 2 == 0:
