df9bbd16315c6a7c3924fc62effb7a8c0be79521
Asliddin <98747380+asliddin2002developer@users.noreply.github.com>
2022-07-04 10:41:57 +0500

    Time: 200 ms (48.48%), Space: 14.9 MB (41.21%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.py
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.py
@@ -16,24 +16,24 @@ class Solution:
         # Time Complexity: O(n)
         # Space Complexity: O(1)
         
-#         l, r = 0, len(numbers)-1
+        l, r = 0, len(numbers)-1
         
-#         while l < r:
-#             curSum = numbers[l] + numbers[r]
-#             if curSum == target:
-#                 return [l+1, r+1]
-#             elif curSum > target:
-#                 r -= 1
-#             else:
-#                 l += 1
+        while l < r:
+            curSum = numbers[l] + numbers[r]
+            if curSum == target:
+                return [l+1, r+1]
+            elif curSum > target:
+                r -= 1
+            else:
+                l += 1
 
         # Time Complexity: O(n)
         # Space Complexity: O(1) 
             
-        hashmap = {}
-        for i, k in enumerate(numbers):
-            if k in hashmap:
-                return [hashmap[k]+1, i+1]
-            else:
-                hashmap[target-k] = i
+#         hashmap = {}
+#         for i, k in enumerate(numbers):
+#             if k in hashmap:
+#                 return [hashmap[k]+1, i+1]
+#             else:
+#                 hashmap[target-k] = i
             
