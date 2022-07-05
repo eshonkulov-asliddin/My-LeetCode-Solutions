2905f6eb68bd9c6f9f0ae7ccf7abe3b065985ed5
Asliddin <98747380+asliddin2002developer@users.noreply.github.com>
2022-07-05 06:25:46 +0500

    Time: 265 ms (62.05%), Space: 43.3 MB (5.09%) - LeetHub

--- 344-reverse-string/344-reverse-string.py
+++ 344-reverse-string/344-reverse-string.py
@@ -14,8 +14,17 @@ class Solution:
         # Time Complexity: O(n)
         # Space Complexity: O(1)
         
-        l, r = 0, len(s)-1
-        while l < r:
-            s[l], s[r] = s[r], s[l]
-            l +=1
-            r-=1
+        # l, r = 0, len(s)-1
+        # while l < r:
+        #     s[l], s[r] = s[r], s[l]
+        #     l +=1
+        #     r-=1
+        
+        # Recursion
+        # Time Complexity: O(n)
+        # Space Complexity: O(n)
+        def reverse(l, r):
+            if l < r:
+                s[l], s[r] = s[r], s[l]
+                reverse(l+1, r-1)
+        reverse(0, len(s)-1)                
