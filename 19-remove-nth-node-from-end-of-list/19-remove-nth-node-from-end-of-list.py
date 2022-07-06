f3f813ce50e9a75dfb3b445651314ae390b46d0e
Asliddin <98747380+asliddin2002developer@users.noreply.github.com>
2022-07-06 10:13:49 +0500

    Time: 60 ms (27.16%), Space: 13.9 MB (69.43%) - LeetHub

--- 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.py
+++ 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.py
@@ -6,34 +6,56 @@
 class Solution:
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
         
-        len_list = 0
-        cur = head
-        while cur:
-            len_list += 1
-            cur = cur.next
+
+        
+        dummy = ListNode(0, head)
+        left = dummy
+        right = head
+        
+        while right and n != 0:
+            right = right.next
+            n -=1
+        
+
+        while right:
+            left = left.next
+            right = right.next  
+        
+        left.next = left.next.next
+        return dummy.next
+        
+        
+#         Time Complexity: O(n2)
+#         Space Complexity: O(n)
             
-        if len_list <= 1:
-            return 
-        elif len_list == 2:
-            if n == 1:
-                cur = head
-                cur.next = None
-            else:
-                head = head.next
-            return head
-        elif len_list == n:
-            head = head.next
-            return head
-        else:
-            cur = head
-            prev = None
+#         len_list = 0
+#         cur = head
+#         while cur:
+#             len_list += 1
+#             cur = cur.next
+            
+#         if len_list <= 1:
+#             return 
+#         elif len_list == 2:
+#             if n == 1:
+#                 cur = head
+#                 cur.next = None
+#             else:
+#                 head = head.next
+#             return head
+#         elif len_list == n:
+#             head = head.next
+#             return head
+#         else:
+#             cur = head
+#             prev = None
 
-            while cur and len_list != n:
-                prev = cur
-                cur = cur.next
-                len_list -=1
+#             while cur and len_list != n:
+#                 prev = cur
+#                 cur = cur.next
+#                 len_list -=1
 
-            prev.next = cur.next 
-            cur =None
+#             prev.next = cur.next 
+#             cur =None
 
-            return head
+#             return head
