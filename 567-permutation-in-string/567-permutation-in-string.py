a33f7b59d6ecba823e7e3947b336055cc180d62d
Asliddin <98747380+asliddin2002developer@users.noreply.github.com>
2022-07-09 08:14:30 +0500

    Time: 83 ms (84.84%), Space: 14 MB (68.12%) - LeetHub

--- 567-permutation-in-string/567-permutation-in-string.py
+++ 567-permutation-in-string/567-permutation-in-string.py
@@ -3,22 +3,25 @@ class Solution:
         
         # Time Complexity: O(n)
         # Space Complexity: O(1)  
-        
         if len(s1) > len(s2):
             return False
-        
-        s1Count, s2Count = [0] * 26, [0] * 26
+
+        s1Count = [0] * 26
+        s2Count = [0] * 26 
+
         for i in range(len(s1)):
-            s1Count[ord(s1[i]) - ord('a')] +=1
-            s2Count[ord(s2[i]) - ord('a')] +=1
-                    
+            s1Count[ord(s1[i]) - ord('a')] += 1
+            s2Count[ord(s2[i]) - ord('a')] += 1
+
         matches = 0
         for i in range(26):
-            matches += (1 if s1Count[i] == s2Count[i] else 0)  
-            
+            if s1Count[i] == s2Count[i]:
+                matches +=1
+
         l = 0
         for r in range(len(s1), len(s2)):
-            if matches == 26: return True
+            if matches == 26:
+                return True
             
             index = ord(s2[r]) - ord('a')
             s2Count[index] += 1
@@ -26,16 +29,53 @@ class Solution:
                 matches += 1
             elif s1Count[index] + 1 == s2Count[index]:
                 matches -= 1
-             
-            #left side
+            
+            #leftside which will be deleted
             index = ord(s2[l]) - ord('a')
             s2Count[index] -= 1
             if s1Count[index] == s2Count[index]:
                 matches += 1
             elif s1Count[index] - 1 == s2Count[index]:
-                matches -= 1 
+                matches -= 1  
+                
             l += 1
             
+        return  matches == 26 
+        
+        
+#         if len(s1) > len(s2):
+#             return False
+        
+#         s1Count, s2Count = [0] * 26, [0] * 26
+#         for i in range(len(s1)):
+#             s1Count[ord(s1[i]) - ord('a')] +=1
+#             s2Count[ord(s2[i]) - ord('a')] +=1
+                    
+#         matches = 0
+#         for i in range(26):
+#             matches += (1 if s1Count[i] == s2Count[i] else 0)  
+            
+#         l = 0
+#         for r in range(len(s1), len(s2)):
+#             if matches == 26: return True
+            
+#             index = ord(s2[r]) - ord('a')
+#             s2Count[index] += 1
+#             if s1Count[index] == s2Count[index]:
+#                 matches += 1
+#             elif s1Count[index] + 1 == s2Count[index]:
+#                 matches -= 1
+             
+#             #left side
+#             index = ord(s2[l]) - ord('a')
+#             s2Count[index] -= 1
+#             if s1Count[index] == s2Count[index]:
+#                 matches += 1
+#             elif s1Count[index] - 1 == s2Count[index]:
+#                 matches -= 1 
+#             l += 1
+            
                 
-        return matches == 26               
+#         return matches == 26  
+
                 
