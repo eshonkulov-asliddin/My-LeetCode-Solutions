3cff1186232f4548c2d73448a8756d6892c7dab2
Asliddin <98747380+asliddin2002developer@users.noreply.github.com>
2022-07-28 06:59:50 +0500

    Time: 419 ms (5.00%), Space: 18.7 MB (70.57%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.py
@@ -1,17 +1,14 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         '''
-        Time Complexity:
-        Space Complexity:
+        Time Complexity: O(n*m)
+        Space Complexity: O(n*m)
         
         '''
         d = dict()
         res = []
         for i in nums:
-            if i in d:
-                d[i] += 1
-            else:
-                d[i] = 1
+            d[i] = 1 + d.get(i, 0)
                 
         while k > 0:
             max_elem = max(d, key=d.get)
