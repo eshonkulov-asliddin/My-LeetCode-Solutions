9f6a58eda8ff9174558355c4c955cc7331d3b35c
Asliddin <98747380+asliddin2002developer@users.noreply.github.com>
2022-07-28 07:07:15 +0500

    Time: 206 ms (18.97%), Space: 19.7 MB (14.52%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.py
@@ -5,14 +5,31 @@ class Solution:
         Space Complexity: O(n*m)
         
         '''
-        d = dict()
-        res = []
-        for i in nums:
-            d[i] = 1 + d.get(i, 0)
+#         d = dict()
+#         res = []
+#         for i in nums:
+#             d[i] = 1 + d.get(i, 0)
                 
-        while k > 0:
-            max_elem = max(d, key=d.get)
-            res.append(max_elem)
-            d[max_elem] = 0
-            k -= 1
-        return res                
+#         while k > 0:
+#             max_elem = max(d, key=d.get)
+#             res.append(max_elem)
+#             d[max_elem] = 0
+#             k -= 1
+#         return res 
+
+
+        # Time Complexity: O(n)
+        # Space Complexity: O(n)
+        count = {}
+        freq = [[] for i in range(len(nums)+1)]
+
+        for n in nums:
+            count[n] = 1 + count.get(n, 0)
+        for n, c in count.items():
+            freq[c].append(n)
+        res = []
+        for i in range(len(freq) - 1, 0, -1):
+            for n in freq[i]:
+                res.append(n)
+                if len(res) == k:
+                    return res
