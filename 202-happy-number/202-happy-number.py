9c3b7b704679656fe8f6cfd129c076530dbc03f2
Asliddin <98747380+asliddin2002developer@users.noreply.github.com>
2022-08-06 16:14:00 +0500

    Time: 51 ms (58.84%), Space: 13.8 MB (97.14%) - LeetHub

---
+++ 202-happy-number/202-happy-number.py
@@ -0,0 +1,29 @@
+class Solution:
+    
+        
+        
+    def isHappy(self, n: int) -> bool:
+        
+        def get_next(n):
+            total_sum = 0
+            while n > 0:
+                digit = n % 10
+                total_sum += digit**2
+                n //= 10
+            return total_sum    
+                
+        seen = set()
+        
+        while n != 1 and n not in seen:
+            seen.add(n)
+            n = get_next(n)
+        
+        return n == 1
+                
+        
+        
+        
+            
+            
+            
+            
