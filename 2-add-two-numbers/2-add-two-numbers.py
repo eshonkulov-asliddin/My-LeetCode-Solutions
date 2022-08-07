7d67fa2d4c5e72621149601cc00f8d6e54f1867f
Asliddin <98747380+asliddin2002developer@users.noreply.github.com>
2022-08-07 07:30:28 +0500

    Time: 84 ms (76.66%), Space: 13.9 MB (86.75%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.py
+++ 2-add-two-numbers/2-add-two-numbers.py
@@ -18,28 +18,28 @@ class Solution:
         Time Complexity: O(n)
         Space Complexity: O(n)
         '''
-        dummy = ListNode()
-        cur = dummy
-        carry = 0           
+#         dummy = ListNode()
+#         cur = dummy
+#         carry = 0           
             
-        while l1 or l2 or carry:
-            v1 = l1.val if l1 else 0
-            v2 = l2.val if l2 else 0
+#         while l1 or l2 or carry:
+#             v1 = l1.val if l1 else 0
+#             v2 = l2.val if l2 else 0
             
-            val = v1 + v2 + carry
-            carry = val // 10
-            val %= 10
+#             val = v1 + v2 + carry
+#             carry = val // 10
+#             val %= 10
                 
                 
             
-            cur.next = ListNode(val)
+#             cur.next = ListNode(val)
             
-            #update
-            cur = cur.next
-            l1 = l1.next if l1 else None
-            l2 = l2.next if l2 else None
+#             #update
+#             cur = cur.next
+#             l1 = l1.next if l1 else None
+#             l2 = l2.next if l2 else None
             
-        return dummy.next        
+#         return dummy.next        
                  
                 
         
@@ -56,23 +56,24 @@ class Solution:
         
         
         
-#         l1_str = ''
-#         l2_str = ''
-#         while l1 or l2:
-#             if l1:
-#                 l1_str += str(l1.val)
-#             if l2:
-#                 l2_str += str(l2.val)
-#             l1 = l1.next
-#             l2 = l2.next
+        l1_str = ''
+        l2_str = ''
+        while l1 or l2:
+            if l1:
+                l1_str += str(l1.val)
+            if l2:
+                l2_str += str(l2.val)
+            l1 = l1.next if l1 else None
+            l2 = l2.next if l2 else None
             
-#         output = str(int(l1_str[::-1]) + int(l2_str[::-1]))
-#         # print(type(output))
+        output = str(int(l1_str[::-1]) + int(l2_str[::-1]))
+        # print(type(output))
         
-#         dummy = ListNode(0, None)
+        dummy = ListNode()
+        cur = dummy
         
         
-#         for i in reversed(output):
-#             dummy.next = i
-#             dummy = dummy.next
-#         return dummy.next    
+        for i in reversed(output):
+            cur.next = ListNode(i)
+            cur = cur.next
+        return dummy.next    
