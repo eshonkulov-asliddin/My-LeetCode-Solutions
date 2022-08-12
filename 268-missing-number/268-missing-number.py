5e25303701a6cfc8441c76201a10e6da1e9bbbe4
Asliddin <98747380+asliddin2002developer@users.noreply.github.com>
2022-08-12 06:29:55 +0500

    Time: 152 ms (85.39%), Space: 15.6 MB (12.19%) - LeetHub

--- 268-missing-number/268-missing-number.py
+++ 268-missing-number/268-missing-number.py
@@ -4,8 +4,26 @@ class Solution:
         Time Complexity: O(n)
         Space Complexity: O(1)
         '''
-        sum_n = 0
-        for i in range(len(nums)+1):
-            sum_n += i
+#         sum_n = 0
+#         for i in range(len(nums)+1):
+#             sum_n += i
             
-        return sum_n - sum(nums)   
+#         return sum_n - sum(nums)  
+    
+        '''
+        Time Complexity: O(n)
+        Space Complexity: O(n)
+        '''
+        d = dict()
+        for i in range(len(nums)+1):
+            d[i] = 1
+            
+        for i in nums:
+            if i in d:
+                d[i] = 0
+         
+        for k, v in d.items():
+            if v != 0:
+                return k
+        
+        
