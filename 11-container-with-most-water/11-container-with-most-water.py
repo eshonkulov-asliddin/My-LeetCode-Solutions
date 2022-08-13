91061779a711e67f0e1de5c4604b447358787cd3
Asliddin <98747380+asliddin2002developer@users.noreply.github.com>
2022-08-13 07:30:50 +0500

    Time: 809 ms (87.76%), Space: 27.5 MB (54.47%) - LeetHub

---
+++ 11-container-with-most-water/11-container-with-most-water.py
@@ -0,0 +1,41 @@
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        
+        '''
+        Two Pointer
+        Time Complexity: O(n)
+        Space Complexity: O(1)
+        '''
+        max_water = 0
+        l, r = 0, len(height)-1
+        while l < r:
+            distance = r - l
+            vertical_line = min(height[l], height[r])
+            
+            area = distance * vertical_line
+            max_water = max(area, max_water)
+            if height[l] < height[r]:
+                l += 1
+                
+            else:
+                r -= 1
+        return max_water                
+        
+        
+        '''
+        Brute Force
+        Time Complexity: O(n2)
+        Space Complexity: O(1)
+        
+        '''
+#         max_water = 0
+        
+#         for i in range(len(height)-1):
+#             for j in range(i+1, len(height)):
+#                 distance = j - i
+#                 vertical_line = min(height[i], height[j])
+                
+#                 area = distance * vertical_line
+                
+#                 max_water = max(max_water, area)
+#         return max_water                
