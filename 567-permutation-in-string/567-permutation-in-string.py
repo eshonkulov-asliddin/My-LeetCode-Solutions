ebf5168b2a3328d88f1a47496670c3b0132d14d5
Asliddin <98747380+asliddin2002developer@users.noreply.github.com>
2022-08-16 09:02:45 +0500

    Time: 137 ms (48.45%), Space: 14.1 MB (6.94%) - LeetHub

--- 567-permutation-in-string/567-permutation-in-string.py
+++ 567-permutation-in-string/567-permutation-in-string.py
@@ -1,6 +1,82 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
         
+        if len(s2) < len(s1):
+            return False
+        
+        s1Count = [0] * 26
+        s2Count = [0] * 26
+        
+        for i in range(len(s1)):
+            s1Count[ord(s1[i]) - ord('a')] += 1
+            s2Count[ord(s2[i]) - ord('a')] += 1
+            
+        matches = 0
+        
+        for i in range(26):
+            if s1Count[i] == s2Count[i]:
+                matches += 1
+                
+        l = 0
+        for r in range(len(s1), len(s2)):
+            
+            if matches == 26:
+                return True
+            
+            index = ord(s2[r]) - ord('a')
+            s2Count[index] += 1
+            
+            if s2Count[index] == s1Count[index]:
+                matches += 1
+            elif s1Count[index] + 1 == s2Count[index]:
+                matches -= 1
+                
+             # leftside will be deleted
+            
+            index = ord(s2[l]) - ord('a')
+            s2Count[index] -= 1
+            
+            if s1Count[index] == s2Count[index]:
+                matches += 1
+            elif s2Count[index] + 1 == s1Count[index]:
+                matches -= 1
+                
+            l += 1
+        return matches == 26           
+                
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
         # Time Complexity: O(n)
         # Space Complexity: O(1)  
         if len(s1) > len(s2):
