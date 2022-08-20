d3900b1fa2357523dbfe73dac6471e3e4a54ac93
Asliddin <98747380+asliddin2002developer@users.noreply.github.com>
2022-08-20 12:21:05 +0500

    Time: 1115 ms (90.96%), Space: 25 MB (38.55%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
@@ -6,26 +6,74 @@ class Solution:
         Time Complexity: O(n)
         Space Complexity: O(1)
         '''
-        n = len(prices)
-        l, r = 0, 1
-        
         max_profit = 0
-        while r < n:
-            if prices[l] < prices[r]:
-                profit = prices[r] - prices[l]
-                max_profit = max(profit, max_profit)
-            else:    
-                l = r
-            r += 1
-
-        return max_profit    
-               
+        l = 0
+        for r in range(len(prices)):
+            if prices[r] < prices[l]:
+                prices[l] = prices[r]
+            profit = prices[r] - prices[l]
+            max_profit = max(max_profit, profit)
+        return max_profit            
+        
+        
+        
         '''
         Brute Force
         Time Complexity: O(n2)
-        Space Comlexity: O(1)
+        Space Complexity: O(1)
         
+        '''        
+#         max_profit = 0
+        
+#         for i in range(len(prices)):
+#             for j in range(i+1, len(prices)):
+#                 profit = prices[j] - prices[i]
+#                 max_profit = max(max_profit, profit)
+#         return max_profit                
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    '''
+        Two Pointer
+        Time Complexity: O(n)
+        Space Complexity: O(1)
         '''
+#         n = len(prices)
+#         l, r = 0, 1
+        
+#         max_profit = 0
+#         while r < n:
+#             if prices[l] < prices[r]:
+#                 profit = prices[r] - prices[l]
+#                 max_profit = max(profit, max_profit)
+#             else:    
+#                 l = r
+#             r += 1
+
+#         return max_profit    
+               
+    
         
         # max_profit = 0
         # for i in range(len(prices)):
