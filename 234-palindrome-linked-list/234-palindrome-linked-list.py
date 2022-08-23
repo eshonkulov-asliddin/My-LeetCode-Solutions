246392cad4531bfdc66c78e32a9dd0bd816f1b46
Asliddin <98747380+asliddin2002developer@users.noreply.github.com>
2022-08-23 08:00:32 +0500

    Time: 1748 ms (6.13%), Space: 128.6 MB (5.06%) - LeetHub

--- 234-palindrome-linked-list/234-palindrome-linked-list.py
+++ 234-palindrome-linked-list/234-palindrome-linked-list.py
@@ -5,13 +5,41 @@
 #         self.next = next
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        
+        '''
+        Recursion
+        Time Complexity: O(n)
+        Space Complexity: O(n)
+        '''
+
+        self.front_pointer = head
+
+        def recursively_check(current_node=head):
+            if current_node is not None:
+                if not recursively_check(current_node.next):
+                    return False
+                if self.front_pointer.val != current_node.val:
+                    return False
+                self.front_pointer = self.front_pointer.next
+            return True
+
+        return recursively_check()
+        
+        
+        
+            
+        
+            
+        
+        
+        
         '''
         Time Complexity: O(n)
         Space Complexity: O(n)
         '''
-        values = []
-        cur = head
-        while cur:
-            values.append(cur.val)
-            cur = cur.next
-        return values == values[::-1]    
+        # values = []
+        # cur = head
+        # while cur:
+        #     values.append(cur.val)
+        #     cur = cur.next
+        # return values == values[::-1]    
