fa7eef12db68dee7277f2c413ed80dea2ecef125
Asliddin <98747380+asliddin2002developer@users.noreply.github.com>
2022-08-25 08:02:07 +0500

    Time: 72 ms (21.06%), Space: 14 MB (32.84%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.py
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.py
@@ -7,115 +7,40 @@ class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
         
         
-        if not list1 and not list2:
-            return
+        dummy = ListNode(0)
+        cur = dummy
         
-        l1 = list1
-        l2 = list2
-        q = None
-        
-        if not l1 :
-            return l2
-        if not l2:
-            return l1
-        
-        if l1 and l2:
-            if l1.val <= l2.val:
-                q = l1
-                l1 = q.next
+        while list1 and list2:
+            if list1.val <= list2.val:
+                cur.next = list1
+                
+                list1 = list1.next
+                cur = cur.next
             else:
-                q = l2
-                l2 = q.next
-            new_head = q
+                cur.next = list2
+                
+                list2 = list2.next
+                cur = cur.next
+        if list1:
+            cur.next = list1
+        if list2:
+            cur.next = list2
             
-        while l1 and l2:
-            if l1.val <= l2.val:
-                q.next = l1
-                q = l1
-                l1 = q.next
-            else:
-                q.next = l2
-                q = l2
-                l2 = q.next
-               
-        if l1 is None:
-            q.next = l2
-        if l2 is None:
-            q.next = l1
-        self.head = new_head
-        return self.head   
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-#         l1 = list1
-#         l2 = list2
-#         q = None
-        
-#         if l1 is None:
-#             return l2
-#         if l2 is None:
-#             return l1
-        
-#         if l1 and l2:
-#             if l1.val <= l2.val:
-#                 q = l1
-#                 l1 = q.next
-#             else:
-#                 q = l2
-#                 l2 = q.next
-#             new_head = q
-           
-#         while l1 and l2:
-#             if l1.val <= l2.val:
-#                 q.next = l1
-#                 q = l1
-#                 l1 = q.next
-#             else:
-#                 q.next = l2
-#                 q = l2
-#                 l2 = q.next
-               
-#         if l1 is None:
-#             q.next = l2
-#         if l2 is None:
-#             q.next = l1
-#         self.head = new_head
-#         return self.head
-        
-#         l1 = []
-#         l2 = []
-#         while list1:
-#             l1.append(list1.val)
-#             list1 = list1.next
+        return dummy.next    
             
-#         while list2:
-#             l2.append(list2.val)
-#             list2 = list2.next
-           
-#         res= l1+l2
-#         res.sort()
         
-#         final_res = None
         
-#         for i in res[::-1]:
-#             final_res = ListNode(val=i, next=final_res)
-            
-        # return final_res
         
-    
-     
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+      
