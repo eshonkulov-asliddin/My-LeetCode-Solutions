321459dd4ff2ced35299872c628b4267643047c5
Asliddin <98747380+asliddin2002developer@users.noreply.github.com>
2022-08-28 15:54:25 +0500

    Time: 370 ms (46.63%), Space: 21.3 MB (53.01%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.py
+++ 238-product-of-array-except-self/238-product-of-array-except-self.py
@@ -6,18 +6,34 @@ class Solution:
         Space Complexity: O(1)
 
         '''
-        res = [1] * len(nums)
-        prefix = 1
-        for i in range(len(nums)):
-            res[i] = prefix
-            prefix *= nums[i]
+        n = len(nums)
         
-        postfix = 1
-        for i in range(len(nums) -1, -1, -1):
-            res[i] *= postfix
-            postfix *= nums[i]
+        if n <= 1:
+            return []
+        productArr = [0] * n
+        product = 1
+        for i in range(len(nums)):
+            productArr[i] = product
+            product *= nums[i]
+        
+        product = 1
+        for i in range(len(nums)-1, -1, -1):
+            productArr[i] *= product
+            product *= nums[i]
             
-        return res    
+        return productArr
+#         res = [1] * len(nums)
+#         prefix = 1
+#         for i in range(len(nums)):
+#             res[i] = prefix
+#             prefix *= nums[i]
+        
+#         postfix = 1
+#         for i in range(len(nums) -1, -1, -1):
+#             res[i] *= postfix
+#             postfix *= nums[i]
+            
+#         return res    
             
             
             
@@ -25,13 +41,13 @@ class Solution:
         Time Complexity: O(n2)
         Space Complexity: O(1)
         '''
-#         res = []
-#         for i in range(len(nums)):
-#             product = 1
-#             for j in range(len(nums)):
-#                 if j != i:
-#                     product *= nums[j]
-#             res.append(product)
-#         return res    
+        # res = []
+        # for i in range(len(nums)):
+        #     product = 1
+        #     for j in range(len(nums)):
+        #         if j != i:
+        #             product *= nums[j]
+        #     res.append(product)
+        # return res    
                     
             
