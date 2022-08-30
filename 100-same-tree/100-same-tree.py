ed15ccc1e425d9d3b5459e03effae696a2ba3390
Asliddin <98747380+asliddin2002developer@users.noreply.github.com>
2022-08-30 10:58:35 +0500

    Time: 56 ms (26.25%), Space: 13.9 MB (29.57%) - LeetHub

--- 100-same-tree/100-same-tree.py
+++ 100-same-tree/100-same-tree.py
@@ -5,29 +5,42 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def preOrder(self, root, res):
-        if root:
-            res.append(root.val)
-            if root.left is None:
-                res.append(None)
-            res = self.preOrder(root.left, res)
+#     def preOrder(self, root, res):
+#         if root:
+#             res.append(root.val)
+#             if root.left is None:
+#                 res.append(None)
+#             res = self.preOrder(root.left, res)
             
-            if root.right is None:
-                res.append(None)
-            res = self.preOrder(root.right, res)
+#             if root.right is None:
+#                 res.append(None)
+#             res = self.preOrder(root.right, res)
         
             
            
-        return res  
+#         return res  
     
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        '''
+        Time Complexity: O(len(p) + len(q))
+        Space Complexity: O(1) if don't consider the recursion space
+        ''' 
+        if not p and not q:
+            return True
+        if not p or not q or p.val != q.val:
+            return False
+        return (self.isSameTree(p.left, q.left) and
+                self.isSameTree(p.right, q.right) )
         
+        '''
+        Second approach
+        Time Complexity: O(p + q)
+        Space Complexity: O(1) if don't consider the recursion space
+        '''
+#         p_l = self.preOrder(p, [])
+#         q_l = self.preOrder(q, [])
         
-        
-        p_l = self.preOrder(p, [])
-        q_l = self.preOrder(q, [])
-        
-        return p_l == q_l
+#         return p_l == q_l
         
         
       
