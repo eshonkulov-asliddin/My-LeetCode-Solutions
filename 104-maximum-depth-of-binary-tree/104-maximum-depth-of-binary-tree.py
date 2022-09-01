653365238a2de76c2c73aad295af3ed93b6f8fa6
Asliddin <98747380+asliddin2002developer@users.noreply.github.com>
2022-09-01 07:11:06 +0500

    Time: 45 ms (90.60%), Space: 15.4 MB (76.48%) - LeetHub

---
+++ 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.py
@@ -0,0 +1,94 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+       # bfs iterative
+        if not root:
+            return 0
+        stack = [(root, 1)]
+        maxDepth = 0
+        while stack:
+            node, depth = stack.pop()
+            maxDepth = max(maxDepth, depth)
+            if node.left:
+                stack.append((node.left, depth + 1))
+            if node.right:
+                stack.append((node.right, depth + 1))
+        return maxDepth   
+                
+       
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    # if not root:
+        #     return 0
+#         else:
+#             return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
+
+            
+# #         stack = [root]
+# #         level = 0
+        
+# #         while len(stack) > 0:
+            
+# #             for i in range(len(stack)):
+# #                 node = stack.pop(0)
+                
+            
+# #                 if node.left:
+# #                     stack.append(node.left)
+# #                 if node.right:
+# #                     stack.append(node.right)
+# #             level += 1    
+          
+            
+# #         return level
+
+#         stack = [[root, 1]]
+#         res = 0
+        
+#         while stack:
+#             node, depth = stack.pop()
+#             if node:
+#                 res = max(res, depth)
+#                 stack.append([node.left, depth+1])
+#                 stack.append([node.right, depth+1])
+#         return res        
+
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+            
