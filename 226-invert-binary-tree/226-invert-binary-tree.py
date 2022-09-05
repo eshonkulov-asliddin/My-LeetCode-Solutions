1c430006d82ad88e42040db7533c5003bc0a591a
Asliddin <98747380+asliddin2002developer@users.noreply.github.com>
2022-09-05 14:01:08 +0500

    Time: 66 ms (9.12%), Space: 13.9 MB (12.15%) - LeetHub

--- 226-invert-binary-tree/226-invert-binary-tree.py
+++ 226-invert-binary-tree/226-invert-binary-tree.py
@@ -12,24 +12,32 @@ class Solution:
         '''
         if not root:
             return
-        stack = [root]
-        while stack:
-            node = stack.pop()
-            if node.left and node.right:
-                tmp = node.left
-                node.left = node.right
-                node.right = tmp
-                stack.append(node.left)
-                stack.append(node.right)
-            elif node.left:
-                node.right = node.left
-                node.left = None
-                stack.append(node.right)
-            elif node.right:
-                node.left = node.right
-                node.right = None
-                stack.append(node.left)
+        # recursive dfs
+        tmp = root.left
+        root.left = root.right
+        root.right = tmp
+        left = self.invertTree(root.left) 
+        right = self.invertTree(root.right)
+        return root
+#         #iterative dfs
+#         stack = [root]
+#         while stack:
+#             node = stack.pop()
+#             if node.left and node.right:
+#                 tmp = node.left
+#                 node.left = node.right
+#                 node.right = tmp
+#                 stack.append(node.left)
+#                 stack.append(node.right)
+#             elif node.left:
+#                 node.right = node.left
+#                 node.left = None
+#                 stack.append(node.right)
+#             elif node.right:
+#                 node.left = node.right
+#                 node.right = None
+#                 stack.append(node.left)
                 
             
         
-        return root
+#         return root
