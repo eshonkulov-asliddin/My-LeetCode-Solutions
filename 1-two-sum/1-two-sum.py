6db713b30b3062b20dd8241c539df45f59d50f0f
Asliddin <98747380+asliddin2002developer@users.noreply.github.com>
2022-09-06 10:22:59 +0500

    Time: 120 ms (46.92%), Space: 15.3 MB (13.70%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -1,23 +1,14 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         '''
-        Time Complexity: O(n2)
-        Space Complexity: O(1)
+        O(n)
+        O(n)
         '''
+        d = dict()
+        for idx, val in enumerate(nums):
+            if val in d:
+                return [idx, d[val]]
+            curKey = target - val
+            d[curKey] = idx
         
-        # for i in range(len(nums)):
-        #     for j in range(i+1, len(nums)):
-        #         if nums[i] + nums[j] == target:
-        #             return [i, j]
-        
-        
-        # Hashmap
-        # Time Complexity: O(n)
-        # Space Complexity: O(n)
-        hashmap = dict()
-        for i in range(len(nums)):
-            if nums[i] in hashmap:
-                return [i, hashmap[nums[i]]]
-            else:
-                hashmap[target-nums[i]] = i
         
