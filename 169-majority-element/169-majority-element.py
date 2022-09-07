3b55ed1e60f3a1ea3cd0f73c9d964a8291a62c63
Asliddin <98747380+asliddin2002developer@users.noreply.github.com>
2022-09-07 10:25:02 +0500

    Time: 395 ms (5.22%), Space: 15.6 MB (34.29%) - LeetHub

--- 169-majority-element/169-majority-element.py
+++ 169-majority-element/169-majority-element.py
@@ -1,15 +1,26 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
         '''
-        Time Complexity: O(n+m)
-        Space Complexity: O(n)
+        Time Complexity: O(n)
+        Space Complexity: O(1)
         '''
-        d = {}
-        maxCount, res = 0, 0
+        res, count = 0, 0
         for num in nums:
-            d[num] = 1 + d.get(num, 0)
-            res = num if d[num] > maxCount else res
-            maxCount = max(d[num], maxCount)
-        return res
+            if num != res:
+                if count:
+                    count -= 1
+                else:
+                    res = num
+                    count = 1
+            else:
+                count += 1
+        return res        
+        # d = {}
+        # maxCount, res = 0, 0
+        # for num in nums:
+        #     d[num] = 1 + d.get(num, 0)
+        #     res = num if d[num] > maxCount else res
+        #     maxCount = max(d[num], maxCount)
+        # return res
         
         
