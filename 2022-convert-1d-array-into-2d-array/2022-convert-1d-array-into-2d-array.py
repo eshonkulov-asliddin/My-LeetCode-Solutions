a36ff390ca4c1a89545abbe0e34c41c8a169b63c
Asliddin <98747380+asliddin2002developer@users.noreply.github.com>
2022-09-08 08:45:03 +0500

    Time: 1872 ms (23.93%), Space: 20.7 MB (99.39%) - LeetHub

--- 2022-convert-1d-array-into-2d-array/2022-convert-1d-array-into-2d-array.py
+++ 2022-convert-1d-array-into-2d-array/2022-convert-1d-array-into-2d-array.py
@@ -1,16 +1,25 @@
 class Solution:
     def construct2DArray(self, original: List[int], m: int, n: int) -> List[List[int]]:
+        '''
+        Time Complexity: O(n)
+        Space Complexity: O(1)
+        '''
         res = []
         if m * n < len(original) or m*n > len(original):
             return []
-        
-        newArr = []
-        i = 0
-        for row in range(m):
-            colm = []
-            for col in range(n):
-                colm.append(original[i])
-                i += 1
-            newArr.append(colm) 
-        return newArr    
+        #aproach2
+        res = []
+        for i in range(0, n*m, n):
+            res.append(original[i:i+n])
+        return res
+        #Aproach1 
+        # newArr = []
+        # i = 0
+        # for row in range(m):
+        #     colm = []
+        #     for col in range(n):
+        #         colm.append(original[i])
+        #         i += 1
+        #     newArr.append(colm) 
+        # return newArr    
          
