18c77df7d8400f950eb9d7e03fb0b6c39283ccf0
Asliddin <98747380+asliddin2002developer@users.noreply.github.com>
2022-09-11 10:44:56 +0500

    Time: 285 ms (12.97%), Space: 14.8 MB (54.31%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.py
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.py
@@ -8,31 +8,56 @@ class Solution:
          [3,4,5,2],
          [1,3,1,5] ]
         """
-        rows = defaultdict(list)
-        cols = defaultdict(list)
+        ROWS, COLS = len(matrix), len(matrix[0])
+        rowZero = False
+        
+        for r in range(ROWS):
+            for c in range(COLS):
+                if matrix[r][c] == 0:
+                    matrix[0][c] = 0
+                    if r > 0:
+                        matrix[r][0] = 0
+                    else:
+                        rowZero = True
+             
+        for r in range(1, ROWS):
+            for c in range(1, COLS):
+                if matrix[0][c] == 0 or matrix[r][0] == 0:
+                    matrix[r][c] = 0
+        if matrix[0][0] == 0:
+            for r in range(ROWS):
+                matrix[r][0] = 0
 
-        row_num = 0
-        col_num = 0
-        for row in matrix:
-            for col in row:
-                if col == 0:
-                    rows[row_num] = [0] * len(row)
-                    cols[col_num] = 0
-                col_num += 1
-            row_num += 1
-            col_num = 0
+        if rowZero:
+            for c in range(COLS):
+                matrix[0][c] = 0
 
-        i, j = 0 ,0 
-        for row in matrix:
-            if i in rows:
-                matrix[i] = rows[i]
-                i += 1
-            else:    
-                for col in row:
-                    if j in cols:
-                        matrix[i][j] = 0
-                    j += 1    
-                i += 1
-                j = 0
-        return matrix    
+                        
+#         rows = defaultdict(list)
+#         cols = defaultdict(list)
+
+#         row_num = 0
+#         col_num = 0
+#         for row in matrix:
+#             for col in row:
+#                 if col == 0:
+#                     rows[row_num] = [0] * len(row)
+#                     cols[col_num] = 0
+#                 col_num += 1
+#             row_num += 1
+#             col_num = 0
+
+#         i, j = 0 ,0 
+#         for row in matrix:
+#             if i in rows:
+#                 matrix[i] = rows[i]
+#                 i += 1
+#             else:    
+#                 for col in row:
+#                     if j in cols:
+#                         matrix[i][j] = 0
+#                     j += 1    
+#                 i += 1
+#                 j = 0
+#         return matrix    
 
