f540d3b84af9c43d7aa3cf4154187c771f83b518
Asliddin <98747380+asliddin2002developer@users.noreply.github.com>
2022-09-13 09:50:45 +0500

    Time: 85 ms (5.43%), Space: 13.7 MB (98.19%) - LeetHub

--- 48-rotate-image/48-rotate-image.py
+++ 48-rotate-image/48-rotate-image.py
@@ -4,19 +4,20 @@ class Solution:
         Do not return anything, modify matrix in-place instead.
         """
         
-        ROWS = COLS = len(matrix)
-        colIdx = 0
-        rowIdx = len(matrix) - 1
-        oldmatrix = matrix
-        res = []
-        for row in range(ROWS):
-            rowIdx = len(matrix) - 1
-            cols = []
-            for col in range(COLS):
-                cols.append(matrix[rowIdx][colIdx])
-                rowIdx -= 1
-            res.append(cols)    
-            colIdx += 1
+        # transpose
+        self.transpose(matrix)
+        # reverse
+        self.reverse(matrix)
+        
+        
+    def transpose(self, matrix):
+        n = len(matrix)
+        for i in range(n):
+            for j in range(i+1, n):
+                matrix[i][j], matrix[j][i] = matrix[j][i],matrix[i][j]
+    def reverse(self, matrix):
+        n = len(matrix)
+        for i in range(n):
+            for j in range(n // 2):
+                matrix[i][j], matrix[i][-j-1] = matrix[i][-j-1], matrix[i][j]
             
-        for i in range(len(matrix)):   
-            matrix[i] = res[i]
