54a643cc426623c174b24f6c1f7e69c8037020d3
Asliddin <98747380+asliddin2002developer@users.noreply.github.com>
2022-09-16 14:43:57 +0500

    Time: 196 ms (7.79%), Space: 15.4 MB (92.16%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.py
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.py
@@ -9,15 +9,15 @@ class Solution:
         i = -1
         while l <= r:
             mid = (l+r) // 2
-            if nums[mid] == target:
+
+            if nums[mid] < target:
+                l = mid + 1
+            elif nums[mid] > target:
+                r = mid - 1
+            else:
                 i = mid
                 if leftBias:
                     r = mid - 1
                 else:
                     l = mid + 1
-
-            elif nums[mid] < target:
-                l = mid + 1
-            elif nums[mid] > target:
-                r = mid - 1
         return i
