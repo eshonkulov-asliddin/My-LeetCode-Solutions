1ed99abacab4e6900b0e4c237782ddc51c5fec09
Asliddin <98747380+asliddin2002developer@users.noreply.github.com>
2022-09-20 11:21:25 +0500

    Time: 41 ms (98.13%), Space: 19.2 MB (15.37%) - LeetHub

--- 113-path-sum-ii/113-path-sum-ii.py
+++ 113-path-sum-ii/113-path-sum-ii.py
@@ -8,34 +8,33 @@ class Solution:
     def pathSum(self, root: Optional[TreeNode], targetSum: int) -> List[List[int]]:
         if not root:
             return
-        res = []
-#         def dfs(node, ans):
-#             if not node.left and not node.right:
-#                 if sum(ans) == targetSum:
-#                     res.append(ans)
-#             if node.left:
-#                 return dfs(node.left, ans+[node.left.val])
-#             if node.right:
-#                 return dfs(node.right, ans+[node.right.val])
-            
-#         dfs(root, [root.val]) 
-#         return res
-        stack = [(root, [root.val])]
-        res = []
-        ans = []
-        while stack:
-            node, ans = stack.pop()
-
+        def dfs(node, ans, res):
             if not node.left and not node.right:
                 if sum(ans) == targetSum:
                     res.append(ans)
+            if node.left:
+                right = dfs(node.left, ans+[node.left.val], res)
+            if node.right:
+                left = dfs(node.right, ans+[node.right.val], res)
+            return res
+        output = dfs(root, [root.val], []) 
+        return output
+#         stack = [(root, [root.val])]
+#         res = []
+#         ans = []
+#         while stack:
+#             node, ans = stack.pop()
+
+#             if not node.left and not node.right:
+#                 if sum(ans) == targetSum:
+#                     res.append(ans)
 
                     
-            if node.left:
-                stack.append((node.left, ans+[node.left.val]))
-            if node.right:
-                stack.append((node.right, ans+[node.right.val]))
-        return res         
+#             if node.left:
+#                 stack.append((node.left, ans+[node.left.val]))
+#             if node.right:
+#                 stack.append((node.right, ans+[node.right.val]))
+#         return res         
                     
                 
             
