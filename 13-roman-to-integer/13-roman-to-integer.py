e012d83ceb5be7754d3723a8f12e3842ff480357
Asliddin <98747380+asliddin2002developer@users.noreply.github.com>
2022-09-24 07:32:41 +0500

    Time: 86 ms (21.90%), Space: 13.5 MB (56.80%) - LeetHub

---
+++ 13-roman-to-integer/13-roman-to-integer.py
@@ -0,0 +1,33 @@
+class Solution(object):
+    def romanToInt(self, s):
+        """
+        :type s: str
+        :rtype: int
+        
+        "MCMXCIV"
+        """
+        
+        roman = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000,
+        }
+        
+        output = 0
+        num = 0
+        for i in range(len(s)-1):
+            if roman[s[i]] >= roman[s[i+1]]:
+                cur = roman[s[i]]
+                output += cur
+            else:
+                cur = roman[s[i]] * -1
+                output += cur
+        
+        output += roman[s[-1]]
+        
+        return output        
+                
